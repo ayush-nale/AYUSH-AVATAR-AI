b13d966672c00a21f3f45a111d7dca47889e9c5c
Ayush Nale <ayushnale@gmail.com>
2026-09-12 02:04:50 +0530

    refactor: completely remove NEXT_PUBLIC prefix requirement

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
-    NEXT_PUBLIC_APP_NAME: process.env.APP_NAME || process.env.NEXT_PUBLIC_APP_NAME,
-    NEXT_PUBLIC_AVATAR_PATH: process.env.AVATAR_PATH || process.env.NEXT_PUBLIC_AVATAR_PATH,
+    SUPABASE_URL: process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY: process.env.SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    APP_NAME: process.env.APP_NAME || process.env.NEXT_PUBLIC_APP_NAME,
+    AVATAR_PATH: process.env.AVATAR_PATH || process.env.NEXT_PUBLIC_AVATAR_PATH,
   }
 };
 
