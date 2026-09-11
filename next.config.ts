1bd2a19c5fe35e3a9fa4b50104404eceb6845796
Ayush Nale <ayushnale@gmail.com>
2026-09-12 01:55:19 +0530

    chore: map env vars to bypass vercel restriction

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: process.env.SUPABASE_PUBLISHABLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_APP_NAME: process.env.APP_NAME || process.env.NEXT_PUBLIC_APP_NAME,
+    NEXT_PUBLIC_AVATAR_PATH: process.env.AVATAR_PATH || process.env.NEXT_PUBLIC_AVATAR_PATH,
+  }
 };
 
 export default nextConfig;
